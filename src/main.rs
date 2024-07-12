b163b48b4a89e63ee89b5873b04c0607eb804cd7
Alex Rowley <alex.rowley@gmail.com>
2024-07-12 19:26:46 +0100

    2 buttons

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,14 @@
 #![no_main]
 
 
-use core::{cmp::Ordering, sync::atomic::AtomicU32};
+use core::{cmp::Ordering, str::Bytes, sync::atomic::AtomicU32};
 
 use defmt_rtt as _;
 use embassy_executor::Spawner;
 use embassy_rp::gpio::{AnyPin, Input, Level, Output, Pin, Pull};
 use embassy_time::{Duration, Timer};
 use panic_probe as _;
+use embassy_sync::{blocking_mutex::raw::ThreadModeRawMutex, pubsub::PubSubChannel};
 
 static SHARED: AtomicU32 = AtomicU32::new(0);
 
@@ -30,7 +31,7 @@ async fn blink_led(pin: AnyPin){
 }
 
 
-#[embassy_executor::task()]
+#[embassy_executor::task(pool_size =2)]
 async fn read_button(pin: AnyPin){
     let mut button = Input::new(pin, Pull::Up);
     loop{
@@ -53,6 +54,7 @@ async fn main(_spawner: Spawner) {
     Timer::after(Duration::from_millis(1000)).await;
     _spawner.spawn(blink_led(peripherals.PIN_14.degrade())).unwrap();
     _spawner.spawn(read_button(peripherals.PIN_13.degrade())).unwrap();
+    _spawner.spawn(read_button(peripherals.PIN_12.degrade())).unwrap();
 
     loop {
         defmt::info!("Main loop");
